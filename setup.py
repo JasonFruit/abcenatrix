62d74bc40539d1a9e4e6ca2cf7660213ac492c15
Jason R. Fruit <JasonFruit@gmail.com>
2017-12-09 19:27:12 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ else:
     
 setup(
     name = "abcenatrix",
-    version = "0.6.6",
+    version = "0.6.7",
     author = "Jason R. Fruit",
     author_email = "jasonfruit@gmail.com",
     description = "A viewer, player, and editor for tunebooks in ABC musical notation.",
